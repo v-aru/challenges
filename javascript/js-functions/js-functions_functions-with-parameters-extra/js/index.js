411522a2848195873f5bcdc02c63b06851361ac6
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-05 15:43:16 +0200

    Update index.js for functions with params extra project

--- javascript/js-functions/js-functions_functions-with-parameters-extra/js/index.js
+++ javascript/js-functions/js-functions_functions-with-parameters-extra/js/index.js
@@ -6,7 +6,9 @@ console.clear();
    The formula is: square = number * number
 */
 
-function printSquare(number) {}
+function printSquare(number) {
+   console.log(`The formula is: square = ${number} * ${number}`);
+}
 
 printSquare(3);
 printSquare(5);
@@ -19,8 +21,11 @@ printSquare(5);
 
 // Uncomment the following function calls and implement the function printCircumference
 
-// printCircumference(4);
-// printCircumference(6);
+function printCircumference (radius) {
+   console.log(`The formula is: circumference = 2 * Pi * ${radius}`);
+}
+ printCircumference(4);
+ printCircumference(6);
 
 /*
 3: We want to use a function which accepts the width and length of a rectangle
@@ -32,5 +37,9 @@ printSquare(5);
 
 // Uncomment the following function calls and implement the function printRectangleArea
 
-// printRectangleArea(5, 7);
-// printRectangleArea(3, 4);
+function printRectangleArea (width, length) {
+   console.log(`The formula is: area = ${width} * ${length}`);
+}
+
+ printRectangleArea(5, 7);
+ printRectangleArea(3, 4);
