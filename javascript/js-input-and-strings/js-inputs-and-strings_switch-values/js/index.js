493ab6a4cbcfc217ef777252c6b2ea559978934f
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-07 10:46:42 +0200

    Update toggle project

--- javascript/js-input-and-strings/js-inputs-and-strings_switch-values/js/index.js
+++ javascript/js-input-and-strings/js-inputs-and-strings_switch-values/js/index.js
@@ -14,13 +14,23 @@ Hint: the HTML entity for the switch button is: &updownarrow
 */
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
+
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const toggleTextButton = document.querySelector('[data-js="toggle-text"]');
+
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
+  secondInput.value = secondInput.value.toUpperCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.toLowerCase();
+});
+
+toggleTextButton.addEventListener("click", () => {
+  [firstInput.value, secondInput.value] = [secondInput.value, firstInput.value]; 
 });
