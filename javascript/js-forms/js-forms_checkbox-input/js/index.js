96c672c56b57df0c9a79b0e1f02d7dec6dfd9b2b
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-07 16:06:24 +0200

    Update js forms checkbox input

--- javascript/js-forms/js-forms_checkbox-input/js/index.js
+++ javascript/js-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,11 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
+
+hideTosError();
+hideSuccessMessage();
+
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +17,36 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function hideSuccessMessage() {
+  successMessage.setAttribute("hidden", "");
+}
+
+function showSuccessMessage() {
+  successMessage.removeAttribute("hidden");
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  if (!tosCheckbox.checked) {
+    showTosError();
+    hideSuccessMessage();
+    return;
+  }
 
+  hideTosError();
+  showSuccessMessage();
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
   alert("Form submitted");
+});
+
+tosCheckbox.addEventListener("change", () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
 });
