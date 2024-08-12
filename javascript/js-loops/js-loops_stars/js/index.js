ddd9665b088939b1305a631e469828109dd78515
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-12 15:43:14 +0200

    Update js starts project

--- javascript/js-loops/js-loops_stars/js/index.js
+++ javascript/js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,38 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+  for (let star = 1; star <= 5; star++) {
+    const img = document.createElement("img");
+    img.src = (star <= filledStars) ? ('./assets/star-filled.svg') : ('./assets/star-empty.svg');
+    
+    img.dataset.index = star;
 
+    img.addEventListener('click', () => {
+      const userStars = img.dataset.index;
+      renderStars(userStars);
+    });
+
+    starContainer.append(img);
+
+
+  }
   //--^-- your code here --^--
 }
 
-renderStars();
+// Part 1
+//Without parameter call
+//renderStars();
+
+// Part 2:
+//With parameter call
+// renderStars(4);
+
+// Part 3:
+//With parameter call
+renderStars(0);
+
