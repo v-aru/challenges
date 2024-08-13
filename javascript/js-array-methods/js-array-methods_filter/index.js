14bb4c492b4730b322a59cb34cef76628ef60bf5
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-13 16:55:37 +0200

    Update filter array methods project

--- javascript/js-array-methods/js-array-methods_filter/index.js
+++ javascript/js-array-methods/js-array-methods_filter/index.js
@@ -23,13 +23,21 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+const onlyCardWithIdTwo = cards.filter(card => card.id == "2");
+console.log(onlyCardWithIdTwo);
 
-const allCardsWith3Tags = null;
+const allCardsWith3Tags = cards.filter(card => card.tags.length == 3);
+console.log(allCardsWith3Tags);
 
-const allCardsThatAreNotBookmarked = null;
+const allCardsThatAreNotBookmarked = cards.filter(card => {
+  return ((card.isBookmarked == false) || !card.isBookmarked);
+});
+console.log(allCardsThatAreNotBookmarked);
 
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter(card => {
+  return (card.isBookmarked && (card.tags.includes('html') || card.tags.includes('js')));
+});
+console.log(allCardsWithTagsHTMLOrJSThatAreBookmarked);
 
 export {
   onlyCardWithIdTwo,
