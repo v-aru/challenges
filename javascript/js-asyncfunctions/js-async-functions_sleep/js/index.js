66c739207fd3c4242211ae1175f97486618a8555
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-16 11:05:48 +0200

    Update sleep js async functions project

--- javascript/js-asyncfunctions/js-async-functions_sleep/js/index.js
+++ javascript/js-asyncfunctions/js-async-functions_sleep/js/index.js
@@ -8,18 +8,25 @@ const body = document.body;
 
 button3.addEventListener("click", async () => {
   icon.textContent = "💤";
-  sleep(3);
+  await sleep(3);
   icon.textContent = "⏰";
 });
 
-button5.addEventListener("click", () => {
+button5.addEventListener("click", async () => {
   icon.textContent = "💤";
+  body.classList.add("dark");
+  await sleep(5);
   icon.textContent = "⏰";
+  body.classList.remove("dark")
 });
 
 function handleSleepFor10Button() {
   icon.textContent = "💤";
-  icon.textContent = "⏰";
+  body.classList.add("dark");
+  sleep(10).then(() => {
+    icon.textContent = "⏰";
+    body.classList.remove("dark");
+  })
 }
 
 button10.addEventListener("click", handleSleepFor10Button);
