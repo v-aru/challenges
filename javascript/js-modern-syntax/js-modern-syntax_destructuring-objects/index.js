625c68166986c64014852fc61a402cb6f09fe06c
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-19 15:53:10 +0200

    Update destructuring objects

--- javascript/js-modern-syntax/js-modern-syntax_destructuring-objects/index.js
+++ javascript/js-modern-syntax/js-modern-syntax_destructuring-objects/index.js
@@ -11,6 +11,8 @@ const company = {
 };
 
 // example: export const {value1} = myObject;
+export const { course } = company;
+// console.log("Course: ", course);
 
 // EXERCISE 2
 // We have an object called 'user'.
@@ -20,6 +22,11 @@ const company = {
 //  2.3: 'isAdmin' property into the variable 'isAdmin' (false, if no such property exists)
 
 const user = { name: "John", years: 30 };
+
+export const { name, years: age, isAdmin = false} = user;
+// console.log("User name: ", name);
+// console.log("Age is: ", age);
+// console.log("isAdmin is: ", isAdmin);
 
 // EXERCISE 3
 // Extract the variables from the dog object and
@@ -31,6 +38,13 @@ const dog = {
   age: 5,
 };
 
+export const { name: dogName, breed: dogBreed, age: dogAge } = dog;
+// console.log("Dog name is: ", dogName);
+// console.log("Dog breed is: ", dogBreed);
+// console.log("Dog age is: ", dogAge);
+//Pretty format print:
+// console.log(`${dogName} is a ${dogAge} years old ${dogBreed}.`);
+
 // EXERCISE 4
 // Extract the 'lastName' property from the person object as "personLastName".
 // Store all other properties in an object called "moreInformation".
@@ -40,6 +54,9 @@ const person = {
   age: 24,
   firstName: "Alex",
 };
+export const {lastName: personLastName, ...moreInformation} = person;
+// console.log("Last name is: ", lastName);
+// console.log("More Information: ", moreInformation);
 
 // EXERCISE 5
 // Rewrite the following function (logInfo) to use destructuring assignment for the three variables it creates:
@@ -49,17 +66,16 @@ const logInfo = (city) => {
   const country = city.country;
   const numPeople = city.population;
 
-  return `${name} is in ${country} and has ${numPeople} inhabitants in it.`
-}
+  return `${name} is in ${country} and has ${numPeople} inhabitants in it.`;
+};
 
-const destructuringLogInfo = (city) => {
+export const destructuringLogInfo = (city) => {
   // (Hint: Lines 48-50 should be replaced with a single line and you shouldn't change anything else).
   // your code here
-
-
+  const { name, country, population: numPeople } = city;
   // your code here
   return `${name} is in ${country} and has ${numPeople} inhabitants in it.`
 }
 
 // This is how you would call it:
- destructuringLogInfo({ name: "Marseille", country: "France", population: 861635 });
+destructuringLogInfo({ name: "Marseille", country: "France", population: 861635 });
