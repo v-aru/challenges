dd649b68eb2afd803e91056b7feecc12d3a6e5c9
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-19 16:54:54 +0200

    Correct exercise 6

--- javascript/js-modern-syntax/js-modern-syntax_destructuring-arrays/index.js
+++ javascript/js-modern-syntax/js-modern-syntax_destructuring-arrays/index.js
@@ -48,7 +48,7 @@ export const [student1, [student2, student3], [student4, student5]] = nestedStud
 
 const values = [1, 6, 7, 9, 12, 5, 4];
 
-export const result = add(1, 6, 7, 9, 12, 5, 4); // Spread values inside this function call
+export const result = add(...values); // Spread values inside this function call
 
 function add(...values) {
   return values.reduce(
