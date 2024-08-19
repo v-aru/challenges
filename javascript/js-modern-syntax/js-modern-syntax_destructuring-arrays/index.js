91b2330eb2af32c2db3220e598e1fcd881a1c0f9
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-19 16:12:52 +0200

    Update destructuring arrays

--- javascript/js-modern-syntax/js-modern-syntax_destructuring-arrays/index.js
+++ javascript/js-modern-syntax/js-modern-syntax_destructuring-arrays/index.js
@@ -5,31 +5,42 @@ console.clear();
 // Make sure to export your variable as in the example below to make the tests work.
 
 const item = ["Egg", 0.25, 12];
-
+export const [name, price, quantity] = item;
+// console.log(`${quantity} ${name}'s is ${price} currency.`);
 // example: export const [value1, value2] = array;
 
 // EXERCISE 2
 // Use array destructuring to extract the variables "firstNameOfChris", "lastNameOfChris" and "ageOfChris".
 
 const personChris = [12, "Chris", "Owen"];
+export const [ageOfChris, firstNameOfChris, lastNameOfChris] = personChris;
+// console.log(`${firstNameOfChris} ${lastNameOfChris} is ${ageOfChris} years old`);
 
 // EXERCISE 3
 // Use array destructuring to extract the variables "firstNameOfAlex" and "lastNameOfAlex".
 // Make sure not to have unused variables.
 
 const personAlex = ["Alex", 12, "Main"];
+export const [firstNameOfAlex, , lastNameOfAlex] = personAlex;
+// console.log(`${firstNameOfAlex} ${lastNameOfAlex}`);
 
 // EXERCISE 4
 // Use array destructuring to extract the last name from the array
 // as variable called "lastName".
 
 const students = ["Christina", "Jon", "Alexandare"];
+export const [, , lastName] = students;
+// console.log("Last Name in Students array is: ", lastName);
 
 // EXERCISE 5
 // Use array destructuring to extract all of the names from this nested array.
 // Assign the given order of students to variables called "student1" to "student5".
 
 const nestedStudents = ["Chris", ["Ahmad", "Antigoni"], ["Toby", "Sam"]];
+export const [student1, [student2, student3], [student4, student5]] = nestedStudents;
+// console.log(`
+//   Name of Student1: ${student1}, Name of Student2: ${student2}, Name of Student3: ${student3}, Name of Student4: ${student4}, Name of Student5: ${student5}
+//   `);
 
 // EXERCISE 6
 // Spread the values of the values array into the function call of the add function.
@@ -37,7 +48,7 @@ const nestedStudents = ["Chris", ["Ahmad", "Antigoni"], ["Toby", "Sam"]];
 
 const values = [1, 6, 7, 9, 12, 5, 4];
 
-const result = add(1); // Spread values inside this function call
+export const result = add(1, 6, 7, 9, 12, 5, 4); // Spread values inside this function call
 
 function add(...values) {
   return values.reduce(
@@ -59,6 +70,13 @@ function add(...values) {
 const arr = [1, 2, 3];
 export function reverseArrayWithoutMutating(arr) {
   // write your code here
+  if (arr.length === 0) {
+    return [];
+  }
+
+  const [head, ...tail] = arr;
+  const reversedTail = reverseArrayWithoutMutating(tail);
+  return [...reversedTail, head];
 }
 const reverseArr = reverseArrayWithoutMutating(arr);
 console.log(reverseArr); // Should log [3, 2, 1] without altering arr
@@ -78,6 +96,7 @@ const array1 = [3, 6, 1];
 const array2 = [8, 5, 2];
 export function mergeAndSortArrays(array1, array2) {
   // write your code here
+  return [...array1, ...array2].sort((a,b) => a - b);
 }
 const mergedAndSorted = mergeAndSortArrays(array1, array2);
 console.log(mergedAndSorted); // Should log [1, 2, 3, 5, 6, 8]
