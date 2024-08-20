249144717d5570601f39d095602036a26f5af3f9
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-20 09:56:31 +0200

    Added unit testing functions

--- javascript/js-unit-testing/js-unit-testing_get-animal/index.js
+++ javascript/js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+    if (animal === "cats") {
+        return "I totally love cats!";
+      } else if (animal === "dogs") {
+        return "I like " + animal + "!";
+      } else {
+        return "I do not like animals at all!";
+      }
+}
