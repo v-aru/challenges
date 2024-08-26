7829e7633895f320a70083894a7129bc605f1fc3
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-26 14:03:24 +0200

    Update greeting project

--- react/react-props/react-props_greeting/src/App.jsx
+++ react/react-props/react-props_greeting/src/App.jsx
@@ -1,5 +1,12 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  //return <Greetings courseName ="Go" />;
+  return <Greetings courseName ="JavaScript" />;
+}
+
+const Greetings = ({ courseName }) => {
+  const syllabus = ["HTML", "CSS", "JavaScript", "TypeScript", "React", "Node.JS"];
+  const topicIncluded = syllabus.includes(courseName);
+  return <h1>{ topicIncluded ? `We are learning ${courseName} which is in our Spiced Academy syllabus` : `Sorry ${courseName} is not in our curriculum!`}</h1>;
 }
