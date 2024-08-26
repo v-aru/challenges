08182da2eec2c8a64359442797c0d4bbe354257b
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-26 14:25:42 +0200

    Update smiley project

--- react/react-props/react-props_smiley/src/App.jsx
+++ react/react-props/react-props_smiley/src/App.jsx
@@ -1,5 +1,9 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley isHappy = {false}/>;
 }
+
+const Smiley = ({ isHappy }) => {
+  return (isHappy ? "😄" : "😔");
+};
