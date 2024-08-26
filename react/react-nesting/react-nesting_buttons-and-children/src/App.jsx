d1b5b21e6c0c8c1c4658e432ee9b5cd698eac585
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-26 16:10:50 +0200

    Update buttons and children project

--- react/react-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ react/react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -3,18 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Click Me x1!</Button>
+      <Button>Click Me x2!</Button>
+      <Button>Click Me x3!</Button>
+      <Button>Click Me x4!</Button>
     </main>
   );
 }
 
-function Button() {
+// eslint-disable-next-line react/prop-types
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      { children }
     </button>
   );
 }
