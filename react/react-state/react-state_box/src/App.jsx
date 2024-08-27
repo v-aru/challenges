2bede7a18a28ad0d820127c3290c9944b2669c0d
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-08-27 15:50:08 +0200

    Update react box project

--- react/react-state/react-state_box/src/App.jsx
+++ react/react-state/react-state_box/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
-  let isActive = false;
+  let [isActive, setActiveState] = useState(false);
+  let [buttonText, setButtonText] = useState("Activate");
 
   function handleClick() {
-    isActive = !isActive;
+    setActiveState(!isActive);
+    setButtonText(buttonText === "Activate" ? "Deactivate" : "Activate");
     // Check that the value changes correctly.
     console.log(isActive);
   }
@@ -12,7 +15,16 @@ export default function App() {
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <button onClick={handleClick}>{buttonText}</button>
     </main>
   );
 }
+
+/* ## Hacker Bonus
+
+Check the console and pay attention to the logged value of `isActive`. You will notice, that `console.log()` doesn't log the value you set with the `set` function one line before. If you set `true` it will log `false` and vice versa. This is not a bug, but the fundamental way how state in React works.
+
+Can you explain why this happens?
+
+Answer: When you call setActiveState(!isActive), isActive still holds the old value. React prepares the update, but the component doesn’t re-render immediately. Therefore, the console log inside handleClick will print the previous value of isActive rather than the new one. After the re-render, isActive will hold the updated value.
+*/
