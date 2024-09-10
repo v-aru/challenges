ac554bd38437805b7c399f5ee7093db686e0eb37
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-09-10 12:06:09 +0200

    Update table rez project

--- react/react-state-2/react-state-2_table-reservation/src/App.jsx
+++ react/react-state-2/react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,22 @@
 import Counter from "./components/Counter";
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  const counterIncrement = () => setPeople(people + 1);
+
+  const counterDecrement = () => setPeople((people > 0) ? people - 1 : 0);
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter 
+        onIncrement = {counterIncrement}
+        onDecrement = {counterDecrement}
+      />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
